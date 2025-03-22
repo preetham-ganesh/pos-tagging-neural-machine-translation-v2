c07ac37d6986c0e597eca905fc78ac973aedc394
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2025-03-21 23:02:19 -0400

    [#3] added save_text_file().
    Saves string as text file.

--- src/utils.py
+++ src/utils.py
@@ -94,3 +94,34 @@ def load_text_file(file_name: str, directory_path: str) -> str:
 
     except FileNotFoundError:
         raise FileNotFoundError(f"{file_path} does not exist.")
+
+
+def save_text_file(text: str, file_name: str, directory_path: str) -> str:
+    """Saves string as text file.
+
+    Saves string as text file.
+
+    Args:
+        text: A string for the text that needs to be saved.
+        file_name: A string for the name of the file that needs to be saved.
+        directory_path: A string for the location where the file needs to be saved.
+
+    Returns:
+        A string for the text from the loaded file.
+    """
+    # Checks type of input documents.
+    assert isinstance(text, str), "Variable text should be of type 'str'."
+    assert isinstance(file_name, str), "Variable file_name should be of type 'str'."
+    assert isinstance(
+        directory_path, str
+    ), "Variable directory_path should be of type 'str'."
+
+    # Checks if the following path exists.
+    directory_path = check_directory_path_existence(directory_path)
+
+    # Saves the dictionary or list as a JSON file at the file path location.
+    file_path = os.path.join(directory_path, file_name)
+    with open(file_path, "w") as out_file:
+        out_file.write(text)
+    out_file.close()
+    print(f"{file_name} file saved successfully.")
