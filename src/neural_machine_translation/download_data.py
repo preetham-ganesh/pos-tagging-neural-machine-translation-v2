f325a77478326646f7018dcd22bcc43cf9d90071
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2025-03-21 23:12:15 -0400

    [#3] added download_europarl_dataset().
    Downloads the Europarl dataset for the language given as input by user.

--- src/neural_machine_translation/download_data.py
+++ src/neural_machine_translation/download_data.py
@@ -95,3 +95,56 @@ def download_tatoeba_dataset(language: str) -> None:
             f"Finished downloading Tatoeba dataset for {language}-en in {(time.time() - start_time):.3f} sec."
         )
     print()
+
+
+def download_europarl_dataset(language: str) -> None:
+    """Downloads the Europarl dataset for the language given as input by user.
+
+    Downloads the Europarl dataset for the language given as input by user.
+
+    Args:
+        language: A string for the language the Europarl dataset should be downloaded.
+
+    Returns:
+        None.
+    """
+    # Checks if the language is valid or not.
+    check_language(language)
+
+    # A dictionary for the language-based Europarl dataset links.
+    dataset_links = {
+        "fr": "https://www.statmt.org/europarl/v7/fr-en.tgz",
+        "de": "https://www.statmt.org/europarl/v7/de-en.tgz",
+        "es": "https://www.statmt.org/europarl/v7/es-en.tgz",
+    }
+
+    # Download the compressed file.
+    start_time = time.time()
+
+    # Checks if the following directory path exists.
+    dataset_directory_path = check_directory_path_existence(
+        os.path.join("data", "raw_data", "europarl")
+    )
+
+    # Checks if the file already exists. If yes, then does not download the file.
+    file_path = os.path.join(dataset_directory_path, f"{language}-en.tgz")
+    if os.path.exists(file_path):
+        print(f"{language}-en.tgz already exists.")
+
+    # Else, downloads it and saves it.
+    else:
+        # Sends request for the current language dataset file.
+        response = requests.get(dataset_links[language])
+
+        # Checks if the response has a success code. If not then prints the error message.
+        assert response.status_code == 200, response.text
+
+        # Saves the compressed file in the response as a .tgz file.
+        with open(file_path, "wb") as out_file:
+            out_file.write(response.content)
+        out_file.close()
+
+        print(
+            f"Finished downloading Europarl dataset for {language}-en in {(time.time() - start_time):.3f} sec."
+        )
+    print()
