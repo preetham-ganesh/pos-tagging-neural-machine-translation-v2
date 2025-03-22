dc4bc7e69f20d7606179e7a93e7ce1669e7a178b
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2025-03-22 14:28:06 -0400

    [#7] added load_data() to Dataset class.
    Loads text pairs for the specified dataset split (train, validation, test).

--- src/neural_machine_translation/rnn/dataset.py
+++ src/neural_machine_translation/rnn/dataset.py
@@ -40,3 +40,92 @@ class Dataset(object):
         self.target_language = target_language
         self.dataset_pairs = {"train": list(), "validation": list(), "test": list()}
         self.tokenizer = dict()
+
+    def load_data(self, split_name: str) -> None:
+        """Loads text pairs for the specified dataset split (train, validation, test).
+
+        Loads text pairs for the specified dataset split (train, validation, test) for all available chunks.
+
+        Args:
+            split_name: A string for the name of the split the text belongs to.
+
+        Returns:
+            None.
+        """
+        # Asserts type & value of the arguments.
+        assert isinstance(
+            split_name, str
+        ), "Variable split_name should be of type 'str'."
+        assert split_name in [
+            "train",
+            "validation",
+            "test",
+        ], "Variable split_name should have value as 'train', 'validation' or 'test'."
+
+        # Checks if the following directory path exists.
+        base_directory_path = check_directory_path_existence(
+            os.path.join(
+                "data",
+                "processed_data",
+                f"{self.model_configuration['language']}-en",
+                f"v{self.model_configuration['dataset']['version']}",
+            )
+        )
+
+        # Creates empty dictionary to store the text pairs in current split.
+        self.dataset_pairs[split_name] = {
+            "en": list(),
+            self.model_configuration["language"]: list(),
+        }
+
+        # If split name is 'train', then loads all training data chunks.
+        if split_name == "train":
+            s_id = 0
+            while True:
+                # If file path does not exist, then exits the loop.
+                if not os.path.exists(
+                    os.path.join(
+                        base_directory_path,
+                        f"train_{s_id}.{self.model_configuration['language']}",
+                    )
+                ):
+                    break
+                # Loads current split id's en & eu text as lines.
+                en_lines = load_text_file(
+                    f"train_{s_id}.en", base_directory_path
+                ).split("\n")
+                eu_lines = load_text_file(
+                    f"train_{s_id}.{self.model_configuration['language']}",
+                    base_directory_path,
+                ).split("\n")
+
+                # Iterates across loaded English & European lines, and adds them to the dataset if not empty.
+                for en_text, eu_text in zip(en_lines, eu_lines):
+                    if en_text != "" and eu_text != "":
+                        self.dataset_pairs["train"]["en"].append(en_text)
+                        self.dataset_pairs["train"][
+                            self.model_configuration["language"]
+                        ].append(eu_text)
+                s_id += 1
+
+        # Else, loads validation & test split files.
+        else:
+            # Loads current split's en & eu text as lines.
+            en_lines = load_text_file(f"{split_name}.en", base_directory_path).split(
+                "\n"
+            )
+            eu_lines = load_text_file(
+                f"{split_name}.{self.model_configuration['language']}",
+                base_directory_path,
+            ).split("\n")
+
+            # Iterates across loaded English & European lines, and adds them to the dataset if not empty.
+            for en_text, eu_text in zip(en_lines, eu_lines):
+                if en_text != "" and eu_text != "":
+                    self.dataset_pairs["train"]["en"].append(en_text)
+                    self.dataset_pairs["train"][
+                        self.model_configuration["language"]
+                    ].append(eu_text)
+        print(
+            f"No. of text pairs in the {split_name} split: {len(self.dataset_pairs[split_name]['en'])}"
+        )
