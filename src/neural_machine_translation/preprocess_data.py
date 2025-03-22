ffe4b2beb20ef91a3f42f1c779d3488f0c5610d3
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2025-03-21 23:19:29 -0400

    [#3] added remove_html_markup() to PreprocessDataset class.
    Removes HTML markup components from text.

--- src/neural_machine_translation/preprocess_data.py
+++ src/neural_machine_translation/preprocess_data.py
@@ -163,3 +163,29 @@ class PreprocessDataset(object):
                 f"Finished extracting files from '{self.language}-en.tgz' to {extracted_data_directory_path}."
             )
             print()
+
+    def remove_html_markup(self, text: str) -> str:
+        """Removes HTML markup components from text.
+
+        Removes HTML markup components from text.
+
+        Args:
+            text: A string for the text which needs to be processed.
+
+        Returns:
+            A string for the processed text without HTML markup components.
+        """
+        # Asserts type & values of the arguments.
+        assert isinstance(text, str), "Variable text should be of type 'str'."
+
+        # Creates an object for BeautifulSoup.
+        soup = BeautifulSoup(text, "lxml")
+
+        # Get the text content of all visible elements.
+        text = soup.get_text(strip=True)
+
+        # Remove any leading or trailing spaces.
+        text = text.strip()
+
+        # Replace consecutive whitespace characters with a single space.
+        return " ".join(text.split())
