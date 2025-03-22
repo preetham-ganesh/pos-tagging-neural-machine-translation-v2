5dc33c03f0789304e633175426a350a01952246c
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2025-03-21 23:11:45 -0400

    [#3] added download_tatoeba_dataset().
    Downloads the Tatoeba dataset for the language given as input by user.

--- src/neural_machine_translation/download_data.py
+++ src/neural_machine_translation/download_data.py
@@ -38,3 +38,60 @@ def check_language(language: str) -> None:
         "fr",
         "de",
     ], "Variable language should have value as 'es', 'fr', or 'de'."
+
+
+def download_tatoeba_dataset(language: str) -> None:
+    """Downloads the Tatoeba dataset for the language given as input by user.
+
+    Downloads the Tatoeba dataset for the language given as input by user.
+
+    Args:
+        language: A string for the language the Europarl dataset should be downloaded.
+
+    Returns:
+        None.
+    """
+    # Checks if the language is valid or not.
+    check_language(language)
+
+    # A dictionary for the language-based Europarl dataset links.
+    dataset_links = {
+        "fr": "https://www.manythings.org/anki/fra-eng.zip",
+        "de": "https://www.manythings.org/anki/deu-eng.zip",
+        "es": "https://www.manythings.org/anki/spa-eng.zip",
+    }
+
+    # Adds headers to the request.
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+        + "Chrome/91.0.4472.124 Safari/537.36"
+    }
+
+    # Download the compressed file.
+    start_time = time.time()
+
+    # Checks if the following directory path exists.
+    dataset_directory_path = check_directory_path_existence(
+        os.path.join("data", "raw_data", "tatoeba")
+    )
+
+    # Checks if the file already exists. If yes, then does not download the file.
+    file_path = os.path.join(dataset_directory_path, f"{language}-en.zip")
+    if os.path.exists(file_path):
+        print(f"{language}-en.zip already exists.")
+
+    else:
+        # Sends request for the current language dataset file.
+        response = requests.get(dataset_links[language], headers=headers)
+
+        # Checks if the response has a success code. If not then prints the error message.
+        assert response.status_code == 200, response.text
+
+        # Saves the compressed file in the response as a .zip file.
+        with open(file_path, "wb") as out_file:
+            out_file.write(response.content)
+        out_file.close()
+        print(
+            f"Finished downloading Tatoeba dataset for {language}-en in {(time.time() - start_time):.3f} sec."
+        )
+    print()
