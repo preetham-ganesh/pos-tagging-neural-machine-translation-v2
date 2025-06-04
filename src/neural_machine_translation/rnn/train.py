956ce14354b1d080ce49c0c96d2b1557c2d2a9bd
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2025-06-04 19:09:42 -0400

    [#6] added init() to Train class.
    Trains the RNN-based Neural Machine Translation model based on the configuration.

---
+++ src/neural_machine_translation/rnn/train.py
@@ -0,0 +1,30 @@
+class Train(object):
+    """Trains the RNN-based Neural Machine Translation model based on the configuration."""
+
+    def __init__(
+        self, model_version: str, input_language: str, target_language: str
+    ) -> None:
+        """Creates object attributes for the Train class.
+
+        Creates object attributes for the Train class.
+
+        Args:
+            model_version: A string for the version of the current model.
+            input_language: A string for the language of the input text.
+            target_language: A string for the language of the target text.
+
+        Returns:
+            None.
+        """
+        # Asserts type & value of the arguments.
+        assert isinstance(model_version, str), "Variable model_version of type 'str'."
+        assert isinstance(input_language, str), "Variable input_language of type 'str'."
+        assert isinstance(
+            target_language, str
+        ), "Variable target_language of type 'str'."
+
+        # Initalizes class variables.
+        self.model_version = model_version
+        self.input_language = input_language
+        self.target_language = target_language
+        self.best_validation_loss = None
