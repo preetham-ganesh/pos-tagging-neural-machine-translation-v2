4e65dfee143647ce118c972a28d1945d264656f4
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2025-06-04 19:12:49 -0400

    [#6] added load_model_configuration() to Train class.
    Loads the model configuration file for model version.

--- src/neural_machine_translation/rnn/train.py
+++ src/neural_machine_translation/rnn/train.py
@@ -1,3 +1,8 @@
+import os
+
+from src.utils import load_json_file
+
+
 class Train(object):
     """Trains the RNN-based Neural Machine Translation model based on the configuration."""
 
@@ -28,3 +33,23 @@ class Train(object):
         self.input_language = input_language
         self.target_language = target_language
         self.best_validation_loss = None
+
+    def load_model_configuration(self) -> None:
+        """Loads the model configuration file for model version.
+
+        Loads the model configuration file for model version.
+
+        Args:
+            None.
+
+        Returns:
+            None.
+        """
+        self.home_directory_path = os.getcwd()
+        model_configuration_directory_path = os.path.join(
+            self.home_directory_path,
+            f"configs/neural_machine_translation/{self.input_language}-{self.target_language}",
+        )
+        self.model_configuration = load_json_file(
+            f"v{self.model_version}", model_configuration_directory_path
+        )
